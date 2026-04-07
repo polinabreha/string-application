20e27cab795b7178cd2c47e1e920d5dcb1de2db9
polinabreha <polinabreha@gmail.com>
2026-04-07 14:40:30 -0400

    Added variable amountofitem

--- src/main/java/com/pluralsight/Reciept.java
+++ src/main/java/com/pluralsight/Reciept.java
@@ -3,7 +3,8 @@ package com.pluralsight;
 public class Reciept {
     public static void main(String[] args) {
         String item = "Phone";
+        int amountOfItem = 2;
         double price = 1000.45;
-        System.out.println(" You bought " + item + " for " + price);
+        System.out.println(" You bought " + item + " for " + price*amountOfItem);
     }
 }
