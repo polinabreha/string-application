d939f294eeb1594cf2847f4e1b56eccacb30d7bb
polinabreha <polinabreha@gmail.com>
2026-04-07 14:23:04 -0400

    Added some code

--- src/main/java/com/pluralsight/Reciept.java
+++ src/main/java/com/pluralsight/Reciept.java
@@ -1,4 +1,9 @@
 package com.pluralsight;
 
 public class Reciept {
+    public static void main(String[] args) {
+        String item = "Phone";
+        double price = 1000.45;
+        System.out.println(" You bought " + item + " for " + price);
+    }
 }
--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -1,4 +1,24 @@
 package com.pluralsight;
 
 public class VariableApp {
+    public static void main(String[] args) {
+        String color = "light blue";
+        int yearStartingSchool = 2026;
+        String middleInitial = "I don't have";
+        boolean hasPet = false;
+        String niceMessage = "I believe in you!!!";
+        int numberDaysInWeek = 7;
+        double priceOfCoffee = 4.99;
+        char favouriteLetter = 'P';
+        boolean isItRainy = false;
+        System.out.println(color);
+        System.out.println(yearStartingSchool);
+        System.out.println(middleInitial);
+        System.out.println(hasPet);
+        System.out.println(niceMessage);
+        System.out.println(numberDaysInWeek);
+        System.out.println(priceOfCoffee);
+        System.out.println(favouriteLetter);
+        System.out.println(isItRainy);
+    }
 }
